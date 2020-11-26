73343fceef543681bd52f14cb2b5ef31859fb156
Carlos Mougan <carmougan@gmail.com>
2020-11-26 22:36:03 +0100

    data splitter improvements

--- data_splitter.py
+++ data_splitter.py
@@ -5,27 +5,49 @@ np.random.seed(0)
 
 df = pd.read_csv("data/gx_merged.csv")
 
+# Take out test
 df = df[df.test == 0]
 
+# Create our unique index variable
 df["count_brand"] = df["country"].astype(str) + "-" + df["brand"]
 
-
+# Unique index
 lista = df["count_brand"].unique()
 df["count_brand"].nunique()
 
-split = int(1078 * 0.75)
+# Get the ones that have not 24months
+a = pd.DataFrame(df.groupby(["country", "brand"]).month_num.max()).reset_index()
+a = a[a.month_num < 23]
+a["count_brand"] = a["country"].astype(str) + "-" + a["brand"]
+deformed = a.count_brand.unique()
 
-len(lista[:split])
+buenos = list(set(lista) - set(list(deformed)))
+
+split = int(len(buenos) * 0.75)
+split_train_list = buenos[:split]
+split_valid_list = buenos[split:]
 
 
-train_split = df[df["count_brand"].isin(lista[:split])]
-valid_split = df[~df["count_brand"].isin(lista[:split])]
+len(split_train_list)
+
+len(split_valid_list)
+
+train_split = df[df["count_brand"].isin(split_train_list)]
+valid_split = df[df["count_brand"].isin(split_valid_list)]
 
 train_split = train_split[["country", "brand"]]
 valid_split = valid_split[["country", "brand"]]
 
 
-train_split.drop_duplicates()
+train_split.drop_duplicates().to_csv("data/train_split_noerror.csv", index=False)
+valid_split.drop_duplicates().to_csv("data/valid_split.csv", index=False)
+
+split_train_split_deformed = list(set((split_train_list + list(deformed))))
+
+train_split = df[df["count_brand"].isin(split_train_split_deformed)]
+
+
+train_split = train_split[["country", "brand"]]
+
 
 train_split.drop_duplicates().to_csv("data/train_split.csv", index=False)
-valid_split.drop_duplicates().to_csv("data/valid_split.csv", index=False)
