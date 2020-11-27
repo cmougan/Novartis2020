524ddeb80dd83ec163683d019356fb0fdf0d32a0
david26694 <david26694@gmail.com>
2020-11-27 11:48:55 +0100

    Relative lightgbm

--- models/lgbm.py
+++ models/lgbm.py
@@ -1,4 +1,5 @@
 
+import re
 import numpy as np
 import pandas as pd
 from lightgbm import LGBMRegressor
@@ -13,29 +14,47 @@ from tools.metrics import (
     apply_metrics,
     prep_data_for_metric,
     get_avg_volumes,
-    mean_absolute_percentage_error
 )
 
+from tools.postprocessing import postprocess_submission
 
-def compute_metrics(preds, lower, upper, y, X, avg_volumes):
+offset_name = "last_before_3_after_0"
+
+
+def compute_metrics(preds, lower, upper, y, offset, X, avg_volumes):
 
     id_cols = ["country", "brand"]
 
     prepped_X = prep_data_for_metric(X, avg_volumes)
 
     prepped_X["actuals"] = y
-    prepped_X["forecast"] = preds
-    prepped_X["lower_bound"] = lower
-    prepped_X["upper_bound"] = upper
+    prepped_X["forecast"] = (preds + 1) * offset
+    prepped_X["lower_bound"] = (lower + 1) * offset
+    prepped_X["upper_bound"] = (upper + 1) * offset
 
     return np.mean(abs(prepped_X.groupby(id_cols).apply(apply_metrics)))
 
+def preprocess(X):
+
+    X = X.copy()
+
+    offset = X[offset_name]
+
+    for col in X.columns:
+        if re.match(r".*mean|median", col):
+            X[col] = (X[col] - offset) / offset
+
+    return X
+
 if __name__ == "__main__":
 
-    full_df = pd.read_csv("data/gx_merged_lags_months.csv").drop(columns=["Unnamed: 0"])
+    full_df = pd.read_csv("data/gx_merged_lags_months.csv")
     submission_df = pd.read_csv("data/submission_template.csv")
     train_tuples = pd.read_csv("data/train_split.csv")
     valid_tuples = pd.read_csv("data/valid_split.csv")
+
+    full_df["volume_offset"] = (full_df["volume"] - full_df[offset_name]) / full_df[offset_name]
+    full_df = preprocess(full_df)
 
     test_df = full_df[full_df.test == 1].copy().reset_index(drop=True)
 
@@ -47,20 +66,23 @@ if __name__ == "__main__":
     # TODO: no need for calculation every time
     avg_volumes = get_avg_volumes()
 
-    to_drop = ["volume", "month_name"]
+    to_drop = ["volume", "volume_offset", "month_name"]
     categorical_cols = ["country", "brand", "therapeutic_area", "presentation"]
 
     train_x = train_df.drop(columns=to_drop)
-    train_y = train_df.volume
+    train_y = train_df.volume_offset
+    train_offset = train_df[offset_name]
 
     val_x = val_df.drop(columns=to_drop)
-    val_y = val_df.volume
+    val_y = val_df.volume_offset
+    val_y_raw = val_df.volume
+    val_offset = val_df[offset_name]
 
     test_x = test_df.drop(columns=to_drop)
+    test_offset = test_df[offset_name]
 
     te = TargetEncoder(cols=categorical_cols)
     te_residual = TargetEncoder(cols=categorical_cols)
-    # imputer = SimpleImputer(strategy="mean")
     lgb = LGBMRegressor(
         n_jobs=-1, n_estimators=50, objective="regression_l1"
     )
@@ -91,20 +113,21 @@ if __name__ == "__main__":
     preds_test = pipe.predict(test_x)
     preds_test_residual = pipe_residual.predict(test_x)
 
-    # bounds = [0, 0.01, 0.1, 0.5, 1, 1.5]
-    bounds = [10]
+    bounds = [0, 0.01, 0.1, 0.5, 1, 1.5, 2]
+    # bounds = [10]
 
     min_unc = 1e8
     best_bound = 0
     for bound in bounds:
         print(f"Bound: {bound}")
         metric_pair = compute_metrics(
-                preds,
-                np.maximum(preds - bound * preds_residual, 0),
-                preds + bound * preds_residual,
-                val_y,
-                val_x,
-                avg_volumes
+                preds=preds,
+                lower=preds - bound * preds_residual,
+                upper=preds + bound * preds_residual,
+                y=val_y_raw,
+                offset=val_offset,
+                X=val_x,
+                avg_volumes=avg_volumes
             )
         print(metric_pair)
 
@@ -117,12 +140,13 @@ if __name__ == "__main__":
     print(min_unc)
     print(best_bound)
 
-    submission_df["pred_95_low"] = np.maximum(preds_test - bound * preds_test_residual, 0)
-    submission_df["pred_95_high"] = preds_test + bound * preds_test_residual
-    submission_df["prediction"] = np.maximum(preds_test, 0)
+    # submission_df["pred_95_low"] = np.maximum(preds_test - bound * preds_test_residual, 0)
+    submission_df["pred_95_low"] = (preds_test - bound * preds_test_residual + 1) * test_offset
+    submission_df["pred_95_high"] = (preds_test + bound * preds_test_residual + 1) * test_offset
+    submission_df["prediction"] = (preds_test + 1) * (test_offset)
 
     # print(submission_df[submission_df.prediction < 0])
+    submission_df = postprocess_submission(submission_df)
 
-    submission_df.to_csv("submissions/baseline.csv", index=False)
-
+    # submission_df.to_csv("submissions/baseline_relative_postprocess.csv", index=False)
 
